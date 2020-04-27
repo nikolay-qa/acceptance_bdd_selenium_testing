ff0162dc2202640627e9c0173561c660a3131845
Nikolay Romeiko <nickolay.romeyko@gmail.com>
2020-04-28 01:24:57 +0300

    Finished the project

--- tests/acceptance/steps/content.py
+++ tests/acceptance/steps/content.py
@@ -1,5 +1,7 @@
 import behave
+
 from tests.acceptance.page_model.base_page import BasePage
+from tests.acceptance.page_model.blog_page import BlogPage
 
 behave.use_step_matcher('re')
 
@@ -15,3 +17,16 @@ def step_impl(context, tag_content):
     page = BasePage(context.driver)
     assert page.title.text == tag_content
 
+
+@behave.then('I can see there is a posts section on the page')
+def step_impl(context):
+    page = BlogPage(context.driver)
+    assert page.posts_section.is_displayed()
+
+
+@behave.then('I can see there is a post with title "(.+)" in the posts section')
+def step_impl(context, post_title):
+    page = BlogPage(context.driver)
+    post_with_title = [post for post in page.posts if post.text == post_title]
+    assert len(post_with_title) > 0
+    assert all([post.is_displayed() for post in post_with_title])
--- tests/acceptance/steps/navigation.py
+++ tests/acceptance/steps/navigation.py
@@ -1,8 +1,9 @@
 import behave
 from selenium import webdriver
+
 from tests.acceptance.page_model.home_page import HomePage
 from tests.acceptance.page_model.blog_page import BlogPage
-
+from tests.acceptance.page_model.new_post_page import NewPostPage
 
 behave.use_step_matcher('re')
 
@@ -18,6 +19,13 @@ def step_impl(context):
 def step_impl(context):
     context.driver = webdriver.Chrome()
     page = BlogPage(context.driver)
+    context.driver.get(page.url)
+
+
+@behave.given('I am on the new post page')
+def step_impl(context):
+    context.driver = webdriver.Chrome()
+    page = NewPostPage(context.driver)
     context.driver.get(page.url)
 
 
